fc05421b20f624b4b54fd0c70c72c3e0bacd661e
Temitope Orojo <oluwaseuntemitope13@gmail.com>
2023-08-26 17:14:06 +0100

    last cha

--- geeknest/settings.py
+++ geeknest/settings.py
@@ -115,7 +115,7 @@ WSGI_APPLICATION = 'geeknest.wsgi.application'
 #Production
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'railway',
         'HOST': 'containers-us-west-144.railway.app',
         'PASSWORD': 'vF4anCugqqDQH7n0t0Mj',
@@ -165,7 +165,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'), #for production
+
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 
