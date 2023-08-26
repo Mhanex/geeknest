091a62320aa17ebc51a0fb790085fe52bfcf1acf
Temitope Orojo <oluwaseuntemitope13@gmail.com>
2023-08-26 12:42:31 +0100

    latest7

--- geeknest/settings.py
+++ geeknest/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = Config('THE_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = Config('THE_DEBUG', cast=bool)
+DEBUG = True
 
 ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1']
 
