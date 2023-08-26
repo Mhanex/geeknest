1b2bf1995b60e3b4d5159168ab04351833106ed6
Temitope Orojo <oluwaseuntemitope13@gmail.com>
2023-08-26 10:56:50 +0100

    addtional

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn geeknest.wsgi --log-file -
--- geeknest/settings.py
+++ geeknest/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = config('THE_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('THE_DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1']
 
 
 # Application definition
@@ -166,17 +166,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-] 
-#for production
-STATIC_ROOT = BASE_DIR / "staticfiles"
-STORAGES = {
-    # ...
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static') #for production
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
